18efd7332606ae15f4a152fa5d263962d6007373
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-05-23 14:57:37 +0900

    feat: repl

---
+++ repl/index.ts
@@ -0,0 +1,39 @@
+import * as readline from "readline"
+import { Lexer } from '../lexer';
+
+export function startRepl() {
+  const PROMPT = '>> ';
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: PROMPT,
+  });
+
+  let buf = '';
+
+  rl.prompt();
+  rl.on('line', async (input) => {
+    buf += input;
+    if (input.length) {
+      try {
+        ''
+      } catch (err) {
+        buf += '\n';
+        rl.setPrompt('...');
+        rl.prompt();
+        return;
+      }
+      const lexer = new Lexer(buf);
+      for(let l of lexer){
+        console.dir(l);
+      }
+    } else {
+      buf = '';
+    }
+    rl.setPrompt(PROMPT);
+    rl.prompt();
+  }).on('close', () => {
+    console.log('( ;ᴗ;)ﾉｼ');
+    process.exit(0);
+  });
+}
