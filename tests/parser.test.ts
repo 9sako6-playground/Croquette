07873b9e5c51a63fcd83133263f3775b0f242218
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-05-26 22:06:36 +0900

    refactor: test

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -25,14 +25,12 @@ let mod = 1000000007;
   for (let i = 0; i < 3; ++i) {
     const statement = program.statements[i];
     expect(statement.tokenLiteral()).toEqual('let');
-    expect(statement instanceof LetStatement).toBeTruthy;
-    if (statement instanceof LetStatement) {
-      const letStatement: LetStatement = statement;
-      expect(letStatement.name.value).toEqual(identifiers[i]);
-      expect(letStatement.name.token.literal).toEqual(identifiers[i]);
-    } else {
+    if (!(statement instanceof LetStatement)) {
       throw new Error();
     }
+    const letStatement: LetStatement = statement;
+    expect(letStatement.name.value).toEqual(identifiers[i]);
+    expect(letStatement.name.token.literal).toEqual(identifiers[i]);
   }
 });
 
@@ -91,20 +89,16 @@ test('should parse identifier expression', () => {
 
   expect(program.statements.length).toEqual(1);
   const statement = program.statements[0];
-  expect(statement instanceof ExpressionStatement).toBeTruthy;
-  if (statement instanceof ExpressionStatement) {
-    const expressionStatement: ExpressionStatement = statement;
-    const ident = expressionStatement.expression;
-    expect(ident instanceof Identifier).toBeTruthy;
-    if (ident instanceof Identifier) {
-      expect(ident.value).toEqual('foobar');
-      expect(ident.tokenLiteral()).toEqual('foobar');
-    } else {
-      throw new Error();
-    }
-  } else {
+  if (!(statement instanceof ExpressionStatement)) {
     throw new Error();
   }
+  const expressionStatement: ExpressionStatement = statement;
+  const ident = expressionStatement.expression;
+  if (!(ident instanceof Identifier)) {
+    throw new Error();
+  }
+  expect(ident.value).toEqual('foobar');
+  expect(ident.tokenLiteral()).toEqual('foobar');
 });
 
 test('should parse integer literal expression', () => {
@@ -115,20 +109,16 @@ test('should parse integer literal expression', () => {
 
   expect(program.statements.length).toEqual(1);
   const statement = program.statements[0];
-  expect(statement instanceof ExpressionStatement).toBeTruthy;
-  if (statement instanceof ExpressionStatement) {
-    const expressionStatement: ExpressionStatement = statement;
-    const ident = expressionStatement.expression;
-    expect(ident instanceof IntegerLiteral).toBeTruthy;
-    if (ident instanceof IntegerLiteral) {
-      expect(ident.value).toEqual(57);
-      expect(ident.tokenLiteral()).toEqual('57');
-    } else {
-      throw new Error();
-    }
-  } else {
+  if (!(statement instanceof ExpressionStatement)) {
     throw new Error();
   }
+  const expressionStatement: ExpressionStatement = statement;
+  const ident = expressionStatement.expression;
+  if (!(ident instanceof IntegerLiteral)) {
+    throw new Error();
+  }
+  expect(ident.value).toEqual(57);
+  expect(ident.tokenLiteral()).toEqual('57');
 });
 
 test('should parse prefix-expressions', () => {
@@ -143,28 +133,22 @@ test('should parse prefix-expressions', () => {
 
     expect(program.statements.length).toEqual(1);
     const statement = program.statements[0];
-    expect(statement instanceof ExpressionStatement).toBeTruthy;
-    if (statement instanceof ExpressionStatement) {
-      const exp = statement.expression;
-      expect(exp instanceof PrefixExpression).toBeTruthy;
-      if (exp instanceof PrefixExpression) {
-        expect(exp.operator).toEqual(sample.operator);
-        expect(exp.right instanceof IntegerLiteral).toBeTruthy;
-        if (exp.right instanceof IntegerLiteral) {
-          const integerLiteral = exp.right;
-          expect(integerLiteral.value).toEqual(sample.integerValue);
-          expect(integerLiteral.tokenLiteral()).toEqual(
-            String(sample.integerValue),
-          );
-        } else {
-          throw new Error();
-        }
-      } else {
-        throw new Error();
-      }
-    } else {
+    if (!(statement instanceof ExpressionStatement)) {
       throw new Error();
     }
+    const exp = statement.expression;
+    if (!(exp instanceof PrefixExpression)) {
+      throw new Error();
+    }
+    expect(exp.operator).toEqual(sample.operator);
+    if (!(exp.right instanceof IntegerLiteral)) {
+      throw new Error();
+    }
+    const integerLiteral = exp.right;
+    expect(integerLiteral.value).toEqual(sample.integerValue);
+    expect(integerLiteral.tokenLiteral()).toEqual(
+      String(sample.integerValue),
+    );
   });
 });
 
@@ -186,32 +170,24 @@ test('should parse infix-expressions', () => {
 
     expect(program.statements.length).toEqual(1);
     const statement = program.statements[0];
-    expect(statement instanceof ExpressionStatement).toBeTruthy;
-    if (statement instanceof ExpressionStatement) {
-      const exp = statement.expression;
-      expect(exp instanceof InfixExpression).toBeTruthy;
-      if (exp instanceof InfixExpression) {
-        expect(exp.operator).toEqual(sample.operator);
-        // left
-        expect(exp.left instanceof IntegerLiteral).toBeTruthy;
-        if (exp.left instanceof IntegerLiteral) {
-          expect(exp.left.value).toEqual(sample.leftValue);
-        } else {
-          throw new Error();
-        }
-        // right
-        expect(exp.right instanceof IntegerLiteral).toBeTruthy;
-        if (exp.right instanceof IntegerLiteral) {
-          expect(exp.right.value).toEqual(sample.rightValue);
-        } else {
-          throw new Error();
-        }
-      } else {
-        throw new Error();
-      }
-    } else {
+    if (!(statement instanceof ExpressionStatement)) {
       throw new Error();
     }
+    const exp = statement.expression;
+    if (!(exp instanceof InfixExpression)) {
+      throw new Error();
+    }
+    expect(exp.operator).toEqual(sample.operator);
+    // left
+    if (!(exp.left instanceof IntegerLiteral)) {
+      throw new Error();
+    }
+    expect(exp.left.value).toEqual(sample.leftValue);
+    // right
+    if (!(exp.right instanceof IntegerLiteral)) {
+      throw new Error();
+    }
+    expect(exp.right.value).toEqual(sample.rightValue);
   });
 });
 
